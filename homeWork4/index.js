1cf696d0651f3b485b6d383a8c3437eae5b4a209
kirill <kmeleshkin@mail.ru>
2021-12-22 19:47:01 +0700

    дз №4 Рефакторинг

--- homeWork4/index.js
+++ homeWork4/index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 const fs = require("fs");
+const rl = require("readline");
+const fsPromises = require("fs").promises;
 const inquirer = require("inquirer");
 const inquirerFileTreeSelection = require('inquirer-file-tree-selection-prompt');
-const rl = require("readline");
+
 const arg = process.argv.slice(2);
 let currentDirectory = '';
-const fsPromises = require("fs").promises;
 
 const isFile = (fileName) => {
     return fs.lstatSync(fileName).isFile();
@@ -62,21 +63,24 @@ const cli = (directory) => {
 
 const checkPath = async () => {
     return await fsPromises.stat(arg[0])
+        .then(res => {
+            return res.isDirectory();
+        })
+        .catch(() => {
+            return false
+        })
 }
 
-checkPath().then((res) => {
-    if (res.isDirectory()) {
+const main = async () => {
+    const isDirectory = await checkPath();
+    if (isDirectory) {
         currentDirectory = arg[0];
-        cli(currentDirectory);
     } else {
         currentDirectory = process.cwd();
-        cli(currentDirectory);
     }
-}).catch(
-    () => {
-        currentDirectory = process.cwd();
-        cli(currentDirectory);
-    }
-);
+    cli(currentDirectory);
+}
 
-
+main().catch((err) => {
+    console.log('Ошибка', err)
+})
